438cc04b7c4cb60ba4306129836946d6eb9d9bd7
avni <avniiiiiii07@gmail.com>
2024-04-14 23:39:28 +0530

    o

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from 'react-redux';
-
+import { Provider } from "react-redux";
 import { store } from "./reduxSlice/Store.jsx";
 import "./index.css";
 import App from "./App.jsx";
+
+// Import the CSS file directly
 import "react-toastify/dist/ReactToastify.css";
 
 const root = createRoot(document.getElementById("root"));
