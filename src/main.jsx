5c23f50c817e6ac986968230d83efba9cf7b52b5
avni <avniiiiiii07@gmail.com>
2024-04-14 23:27:28 +0530

    p

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
+
 import { store } from "./reduxSlice/Store.jsx";
 import "./index.css";
 import App from "./App.jsx";
